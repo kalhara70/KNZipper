de7f684905e52293ca2e5e5d458f48952111fe99
Kalhara Nonis <rdamayanthi756@gmail.com>
2024-12-31 21:55:03 +0530

    Update library_script.js

--- library_script.js
+++ library_script.js
@@ -1,5 +1,5 @@
 const webApps = [
-            { name: 'QR Code Generator', description: 'Streamline your workflow and manage projects effectively with our intuitive project management solution.', link: 'qr_code_gen.html' , category: 'Generator' },
+            { name: 'QR Code Generator', description: 'With our QR code generator, you can convert any URL or TEXT into a qr code. You can use it anywhere.', link: 'qr_code_gen.html' , category: 'Generator' },
             { name: 'Passport Photo Croper', description: 'Stay organized and boost productivity with our comprehensive task tracking system.', link: 'passport_zise_photo_crop.html', category: 'Editions' },
             { name: 'Temp Video Player', description: 'Take control of your finances with our smart budget planning and expense tracking tools.', link: 'temp_video_player.html', category: 'Media_Tools' },
             { name: 'Code Editor', description: 'Write, edit, and debug code efficiently with our powerful online development environment.', link: 'https://example.com/code-editor', category: 'development' },
